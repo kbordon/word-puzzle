7d91a7bb4c7fcb70080cb567959266c77d113e4a
Kimberly Bordon and Rakhee Gandhi <rakhee@>
2017-09-19 10:41:28 -0700

    Add form, button, labels in HTML and for loop and variables for input sentence.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#stringInput").submit(function(event){
+
+    var sentenceInput = $("input#string").val();
+    var vowelsArray = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    var sentenceArray = [];
+
+    for ( var index = 0; index < sentenceInput.length; index++) {
+      if (vowelsArray.includes(sentenceInput.charAt(index))) {
+        sentenceArray.push("-");
+      } else {
+        sentenceArray.push(sentenceInput.charAt(index));
+      }
+    }
+
+    alert(sentenceArray);
+
+
+  event.preventDefault();
+  });
+});
